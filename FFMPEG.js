986cb132f05ded05bc0cc2726a40ef0b8b06c7bf
Mauricio Gomes <gomes98@gmail.com>
2025-01-29 16:05:10 -0300

    adicionado a opção de hls, removido o websocket

--- FFMPEG.js
+++ FFMPEG.js
@@ -1,36 +1,85 @@
 const { spawn } = require('child_process');
 const EventEmitter = require('events');
+const path = require('path');
+const fs = require('fs');
 
 class FFMPEG extends EventEmitter {
-  constructor(url, codecData) {
+  constructor(config = {
+    url: '',
+    codecData: null,
+    accept: '',
+    codec_name: '',
+    outputDir: '',
+    id: 0,
+  }) {
     super();
     this.stream = null;
-    this.url = url;
-    this.bitrate = 0;
-    this.bitrateTimer = null;
-    this.codecData = codecData;
-    this.codecsAvailable = [
-      { codec_name: 'h264', bsf: 'h264_mp4toannexb' },
-      { codec_name: 'hevc', bsf: 'hevc_mp4toannexb' },
-      // { codec_name: 'mpeg4', bsf: 'mpeg4_unpack_bframes' },
-      // { codec_name: 'vp8', bsf: 'vp8_superframe' },
-      // { codec_name: 'vp9', bsf: 'vp9_superframe' },
-      // { codec_name: 'av1', bsf: 'av1_superframe' },
-    ];
+    this.id = config.id || new Date().getTime();
+    this.url = config.url;
+    this.codecData = config.codecData;
+    this.outputDir = config.outputDir || path.join(__dirname, 'output');
+    this.tickerTimer = null;
+    this.lastTick;
   }
 
-  start(codec_name) {
+  async startHls() {
+    if (!fs.existsSync(this.outputDir)) {
+      await fs.promises.mkdir(this.outputDir);
+    } else {
+      this.clearDir();
+    }
+    try {
+      await this.checkVideoContent();
+    } catch (error) {
+      this.emit('error', { error, msg: 'Codec not available' });
+    }
+    // montando os argumentos para h264
+    let args = `-re -rtsp_transport tcp -i ${this.url} -c copy -vsync -1 -flags +global_header -hls_list_size 8 -hls_time 2 -hls_flags delete_segments -segment_list_flags +live -f hls ${this.outputDir}/cam_${this.id}_.m3u8`.split(' ');
+    // montando os argumentos para outros codecs
+    if (this.codecData.streams[0].codec_name != 'h264') {
+      args = `-re -rtsp_transport tcp -i ${this.url} -c:v libx264 -preset ultrafast -crf 40 -c:a copy -s 1280x720 -b:v 500k -hls_list_size 8 -hls_time 2 -hls_flags delete_segments -f hls ${this.outputDir}/cam_${this.id}_.m3u8`.split(' ');
+    }
+    // iniciando o processo ffmpeg
+    this.stream = spawn(
+      'ffmpeg',
+      args,
+      { detached: false, windowsHide: true },
+    );
+    // tratamento de erros
+    this.stream.stderr.on('error', (e) => this.emit('error', { error: e, msg: 'err:error' }));
+    this.stream.stdout.on('error', (e) => this.emit('error', { error: e, msg: 'out:error' }));
+    this.stream.on('error', (error) => {
+      this.emit('error', { error, msg: 'ffmpeg error' });
+    });
+    // tratamento de saida
+    this.stream.on('exit', (_code, signal) => {
+      this.stream = null;
+      this.emit('exit', { code: _code, signal, msg: 'ffmpeg exit' });
+    });
+    this.emit('start', { url: this.url, codec: this.codecData });
+    // monitorando o tempo de execução
+    this.tickerTimer = setInterval(() => {
+      let now = new Date().getTime();
+      if (now - this.lastTick > 10000) {
+        this.stop();
+      }
+    }, 1000);
+  }
+
+  async start() {
     let bitrate = 0;
     // vericando se o codec é suportado
-    let bsf;
-    if(!this.codecData && codec_name){
-      bsf = this.codecsAvailable.find(c => c.codec_name === codec_name).bsf;
-    }else{
-      bsf = this.codecsAvailable.find(c => c.codec_name === this.codecData.streams[0].codec_name).bsf;
+    try {
+      await this.checkVideoContent();
+    } catch (error) {
+      this.emit('error', { error, msg: 'Codec not available' });
     }
-    if (!bsf) throw new Error('Codec not available');
-    // montando os argumentos
-    let args = `-i ${this.url} -c:v copy -c:a copy -bsf:v ${bsf} -maxrate 4096k -f matroska -`.split(' ');
+    // montando os argumentos para h264
+    let args = `-re -rtsp_transport tcp -i ${this.url} -c:v copy -c:a copy -f matroska -`.split(' ');
+    // montando os argumentos para outros codecs
+    if (this.codecData.streams[0].codec_name != 'h264') {
+      args = `-re -rtsp_transport tcp -i ${this.url} -c:v libx264 -preset ultrafast -crf 40 -c:a copy -s 1280x720 -f matroska -`.split(' ');
+    }
     this.stream = spawn(
       'ffmpeg',
       args,
@@ -61,28 +110,42 @@ class FFMPEG extends EventEmitter {
     }, 1000);
   }
 
-  stop() {
+  async stop() {
     if (this.stream) {
       this.stream.kill();
       this.stream = null;
-      clearInterval(this.bitrateTimer);
+      // parando o ticker
+      clearInterval(this.tickerTimer);
+      // limpando diretório de saída
+      this.clearDir();
+      this.emit('stop', { msg: 'ffmpeg stop' });
     }
+  }
+
+  async clearDir() {
+    let files = await fs.promises.readdir(this.outputDir)
+    files.filter(f => f.includes(`cam_${this.id}`)).forEach(async (f) => {
+      await fs.promises.unlink(path.join(this.outputDir, f));
+    });
+  }
+
+  tick() {
+    // console.log('tick', this.id);
+    this.lastTick = new Date().getTime();
   }
 
   isRunning() {
     return !!this.stream;
   }
 
-  getCodecName(){
+  getCodecName() {
     return this.codecData.streams[0].codec_name;
   }
 
   async checkVideoContent() {
     try {
-      // if (onlyCodec) return await checkVideoContent(url).then(data => data.streams[0].codec_name);
-      // return await checkVideoContent(url);
       let codecData = await checkVideoContent(this.url);
-      this.codecData = codecData;     
+      this.codecData = codecData;
       return codecData;
     } catch (error) {
       throw error;
@@ -103,9 +166,9 @@ const checkVideoContent = (url) => {
     this.stream.stdout.on('data', (data) => {
       buf = Buffer.concat([buf, data]);
     });
-    this.stream.stderr.on('error', (e) => error = { error: e, msg: 'err:error' });
-    this.stream.stdout.on('error', (e) => error = { error: e, msg: 'out:error' });
-    this.stream.on('error', (error) => error = { error: e, msg: 'ffmpeg:error' });
+    this.stream.stderr.on('error', (error) => error = { error, msg: 'err:error' });
+    this.stream.stdout.on('error', (error) => error = { error, msg: 'out:error' });
+    this.stream.on('error', (error) => error = { error, msg: 'ffmpeg:error' });
     this.stream.on('exit', (_code, signal) => {
       if (_code === 0) {
         let data = JSON.parse(buf.toString());
--- index.js
+++ index.js
@@ -1,56 +1,141 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const FFMPEG = require('./FFMPEG');
-const expressWs = require('express-ws')(app);
 const HTTP_PORT = 2000;
-
-// middleware para logar as requisições
-// app.use((req, res, next) => {
-//   console.log(req.ip, req.url, req.method, '\n');
-//   next();
-// });
-
-
-app.ws('/camera', function (ws, req) {
-  console.log('new client');
-  ws.on('close', () => {
-    console.log('client left');
-  });
-});
-
-const sendToAll = (data) => {
-  expressWs.getWss().clients.forEach(client => {
-    client.send(JSON.stringify(data));
-  });
+const FFMPEG = require('./FFMPEG');
+const outputDir = path.join(__dirname, 'output');
+const timers = require('timers/promises');
+let cameras;
+// Cria o diretório de saída se não existir
+const fs = require('fs');
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+} else {
+  clearDir();
 }
 
-app.get('/camera', async (req, res) => {
-  // validções
-  if (!req.query.b64) return res.status(400).send('b64 is required');
-  let url = Buffer.from(req.query.b64, 'base64').toString('utf8');
-  if (!url) return res.status(400).send('url is required');
+if(fs.existsSync('cameras.json')){
+  cameras = JSON.parse(fs.readFileSync('cameras.json'));
+}else{
+  cameras = [];
+}
 
+async function clearDir() {
+  let files = await fs.promises.readdir(outputDir)
+  files.forEach(async (f) => {
+    await fs.promises.unlink(path.join(outputDir, f));
+  });
+}
+// mostra no console as requisições
+app.use((req, res, next) => {
+  console.log(req.ip, req.url, req.method, '\n');
+  next();
+});
+// ROTA HLS
+let CAMERASON = [];
+app.get('/hls/:fileId', async (req, res) => {
+  let camera;
+  // se não houver ID da câmera, retorna 404
+  if (!req.params.fileId) {
+    return res.status(404).send('Câmera não encontrada');
+  }
+  // extração do ID da câmera
+  let fileId;
+  try {
+    fileId = parseInt(req.params.fileId.split('_')[1]);
+    if (!isNaN(fileId)) {
+      camera = cameras.find(cam => cam.id === fileId);
+      if (!camera) {
+        return res.status(404).send('Câmera não encontrada');
+      }
+    }
+  } catch (error) {
+    console.log('Erro ao extrair o ID da câmera:', error);
+    return res.status(404).send('Câmera não encontrada @');
+  }
+  // se o arquivo não existir e for o m3u8, inicia o processo ffmpeg  
+  if (req.params.fileId.split('.').at(-1) == 'm3u8' && !fs.existsSync(path.join(outputDir, req.params.fileId))) {
+    // prepara o ffmpeg
+    let c = new FFMPEG({
+      url: camera.principal,
+      id: camera.id,
+    });
+    // inicia o ffmpeg
+    c.startHls();
+    // quando sair do ffmpeg, remove do array
+    c.on('stop', () => {
+      CAMERASON = CAMERASON.filter(cam => cam.id != fileId);
+    });
+    // quando sair do ffmpeg, remove do array
+    c.on('exit', () => {
+      CAMERASON = CAMERASON.filter(cam => cam.id != fileId);
+    });
+    // quando der erro, remove do array
+    c.on('error', (e) => {
+      console.log('Erro no ffmpeg:', e);
+      CAMERASON = CAMERASON.filter(cam => cam.id != fileId);
+    });
+    // adiciona ao array
+    CAMERASON.push(c);
+  } else {
+    // informa ao ffmpeg que o arquivo foi acessado, para não parar
+    let c = CAMERASON.find(cam => cam.id == fileId);
+    if (c && typeof c.tick == 'function') {
+      c.tick();
+    }
+  }
+  // pausa para esperar o arquivo ser criado, até 10 segundos
+  let status = false;
+  for (let i = 0; i < 10; i++) {
+    await timers.setTimeout(1000);
+    if (fs.existsSync(path.join(outputDir, req.params.fileId))) {
+      status = true;
+      break;
+    }
+  }
+  if (!status) {
+    return res.status(404).send('Arquivo não encontrado');
+  }
+  res.setHeader('Cache-Control', 'no-cache');
+  return res.sendFile(path.join(outputDir, req.params.fileId));
+});
+// ROTA VIDEO
+app.get('/video/:cameraId', async (req, res) => {
+  let camera;
+  // se não houver ID da câmera, retorna 404
+  if (!req.params.cameraId) {
+    return res.status(404).send('Câmera não informada');
+  }
+  // extração do ID da câmera
+  try {
+    camera = cameras.find(cam => cam.id == req.params.cameraId);
+    if (!camera) {
+      return res.status(404).send('Câmera não encontrada');
+    }
+  } catch (error) {
+    console.log('Erro ao extrair o ID da câmera:', error);
+    return res.status(404).send('Câmera não encontrada @');
+  }
   // se é o primeiro frame
   let first = true;
   // instancia o ffmpeg
-  let ffmpeg = new FFMPEG(url);
-  // verifica o tipo do video
-  await ffmpeg.checkVideoContent();
+  let ffmpeg = new FFMPEG({
+    url: camera.principal,
+    id: camera.id,
+  });
   // inicia o ffmpeg
   ffmpeg.start();
   // escuta os eventos
   ffmpeg.on('data', (data) => {
     if (first) {
       first = false;
-      sendToAll({ event: 'codec', data: ffmpeg.codecData, url: ffmpeg.url });
       res.writeHead(200, {
-        'Content-Type': 'video/mp4',
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'video/x-matroska',
+        // 'Content-Type': 'video/*',
       });
     }
     res.write(data);
-  });
-  ffmpeg.on('bitrate', (e) => {
-    sendToAll({ event: 'bitrate', data: e, url: ffmpeg.url });
   });
   ffmpeg.on('error', (e) => {
     console.log('error', e);
@@ -61,24 +146,39 @@ app.get('/camera', async (req, res) => {
     res.end();
   });
   req.on('close', () => {
-    ffmpeg.stop();
-    ffmpeg.removeAllListeners('data');
-    ffmpeg.removeAllListeners('error');
-    ffmpeg.removeAllListeners('exit');
+    if (ffmpeg) {
+      ffmpeg.stop();
+      ffmpeg.removeAllListeners('data');
+      ffmpeg.removeAllListeners('error');
+      ffmpeg.removeAllListeners('exit');
+      ffmpeg = null;
+    }
     res.end();
-    ffmpeg = null;
   });
   req.on('end', () => {
-    ffmpeg.stop();
-    ffmpeg.removeAllListeners('data');
-    ffmpeg.removeAllListeners('error');
-    ffmpeg.removeAllListeners('exit');
+    if (ffmpeg) {
+      ffmpeg.stop();
+      ffmpeg.removeAllListeners('data');
+      ffmpeg.removeAllListeners('error');
+      ffmpeg.removeAllListeners('exit');
+      ffmpeg = null;
+    }
     res.end();
-    ffmpeg = null;
   });
 });
 
-app.get('/*', express.static('html'));
+
+app.use('/', express.static('html'));
+
+app.get('/cameras', (req, res) => {
+  if (fs.existsSync('cameras.json')) {
+    let cameras = JSON.parse(fs.readFileSync('cameras.json'));
+    res.json(cameras);
+  } else {
+    res.json([]);
+  }
+});
+
 app.listen(HTTP_PORT, () => {
-  console.log('Servidor na url http://localhost:' + HTTP_PORT);
+  console.log(`Servidor rodando em http://localhost:${HTTP_PORT}`);
 });
